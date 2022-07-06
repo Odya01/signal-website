0385727bbfd5ab4740a7f200f2ec239bbc7f4f0d
Odya01 <republiquez25@gmail.com>
2022-07-07 00:01:40 +0300

    end css, add map script

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Яндекс карта
+ymaps.ready(init);
+function init(){
+	var myMap = new ymaps.Map("map", {
+    center: [55.740653, 37.573112],
+    zoom: 16
+  });
+
+	// Метка на карте
+	var myPlacemark = new ymaps.Placemark([55.740653, 37.573112], {}, {
+		preset: 'islands#orangeIcon',
+		iconImageSize: [35, 40],
+		iconImageOffset: [0, -2]
+	});
+
+	myMap.geoObjects.add(myPlacemark); 
+}
